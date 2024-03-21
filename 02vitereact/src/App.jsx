751ceded08590a20e817dc4fddbfeae78f1bb058
Yash-codeAndDev <yashkhati23@gmail.com>
2024-03-21 20:46:56 +0530

    React Basics and element injection

---
+++ 02vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const username = "Yash Khati"
+  return (
+    <>
+        {/* <Chai/> */}
+        <h1>Me and React by {username}</h1>
+        <p>Test Para</p>
+    </>
+  )
+}
+
+export default App
