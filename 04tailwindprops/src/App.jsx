20bd26b418e35a676bdfd8999b5ffe3fbc7adbd4
Yash-codeAndDev <yashkhati23@gmail.com>
2024-04-30 20:14:36 +0530

    props in React

--- 04tailwindprops/src/App.jsx
+++ 04tailwindprops/src/App.jsx
@@ -1,16 +1,14 @@
-import './App.css'
+import { useState } from 'react'
 import Card from './components/Card'
-function App() {
-  let myObj = {
-    username :"Yash",
-    age : 21,
-  }
+import './App.css'
 
-  let newArr = [1,2,3,4,5]
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">Tailwind Test</h1>
-      <Card username="chai aur code" someObj= {myObj} someArr = {newArr}/>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-5' >Tailwind Test</h1>
+      <Card userName = "Yash Khati"/>
       <Card/>
     </>
   )
