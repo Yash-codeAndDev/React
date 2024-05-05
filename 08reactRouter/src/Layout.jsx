67e18db147f7c2b9c5b45fd10d202bcfaa2bfb19
Yash-codeAndDev <yashkhati23@gmail.com>
2024-05-05 11:48:59 +0530

    React Router basics way 1-> createBrowserRouter using array Object

--- 08reactRouter/src/Layout.jsx
+++ 08reactRouter/src/Layout.jsx
@@ -3,12 +3,13 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
+
 function Layout() {
   return (
     <>
-        <Header/>
+        <Header />
         <Outlet/>
-        <Footer/>
+        <Footer/>        
     </>
   )
 }
--- 08reactRouter/src/components/Footer/Footer.jsx
+++ 08reactRouter/src/components/Footer/Footer.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 export default function Footer() {
     return (
         <footer className="bg-white border-y">
-            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+            <div className="w-full max-w-screen-xl p-4 py-6 mx-auto lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <Link to="/" className="flex items-center">
                             <img
                                 src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                                className="mr-3 h-16"
+                                className="h-16 mr-3"
                                 alt="Logo"
                             />
                         </Link>
@@ -17,7 +16,7 @@ export default function Footer() {
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
-                            <ul className="text-gray-500 font-medium">
+                            <ul className="font-medium text-gray-500">
                                 <li className="mb-4">
                                     <Link to="/" className="hover:underline">
                                         Home
@@ -32,7 +31,7 @@ export default function Footer() {
                         </div>
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
-                            <ul className="text-gray-500 font-medium">
+                            <ul className="font-medium text-gray-500">
                                 <li className="mb-4">
                                     <a
                                         href="https://github.com/hiteshchoudhary"
@@ -52,7 +51,7 @@ export default function Footer() {
                         </div>
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
-                            <ul className="text-gray-500 font-medium">
+                            <ul className="font-medium text-gray-500">
                                 <li className="mb-4">
                                     <Link to="#" className="hover:underline">
                                         Privacy Policy
