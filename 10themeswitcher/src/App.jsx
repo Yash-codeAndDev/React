02344b47ddc12c4e389d1f33f99bce07b08db35a
Yash-codeAndDev <yashkhati23@gmail.com>
2024-05-20 17:13:41 +0530

    ContextSwitch Practice using Theme Switcher

--- 10themeswitcher/src/App.jsx
+++ 10themeswitcher/src/App.jsx
@@ -1,41 +1,42 @@
-import { useState } from 'react'
-import './App.css'
-import { ThemeProvider } from './context/Theme'
-import { useEffect } from 'react'
-import ThemeBtn from './components/ThemeBtn'
-import Card from './components/Card'
+import { useState,useEffect } from "react";
+import "./App.css";
+import { ThemeProvider } from "./context/Theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
 function App() {
-  
-  const [themeMode , setThemeMode] = useState('light')
+
+  const [themeMode, setThemeMode] = useState("light")
+
   const lightTheme = () => {
-    setThemeMode('light')
-  }
+    setThemeMode("light")
+  }  
   const darkTheme = () => {
-    setThemeMode('dark')
+    setThemeMode("dark")
   }
-  
 
   useEffect(() => {
-    document.querySelector('html').classList.remove('light','dark')
+    document.querySelector('html').classList.remove("light","dark")
     document.querySelector('html').classList.add(themeMode)
-  },[themeMode])
+  }, [themeMode]);
+
+
 
   return (
-    <ThemeProvider value={ {themeMode, lightTheme, darkTheme} }>
-      <div className="flex flex-wrap min-h-screen items-center">
-          <div className="w-full">
-            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">   
-            
-              <ThemeBtn/>
-               </div>
-
-                <div className="w-full max-w-sm mx-auto">
-              <Card/> 
-              </div>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap items-center min-h-screen">
+        <div className="w-full">
+          <div className="flex justify-end w-full max-w-sm mx-auto mb-4">
+            <ThemeBtn/>
           </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
+        </div>
       </div>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
